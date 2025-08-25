d63daf0fcfb96a3782f11829b25a88bb28b70e2f
aayushsoodhp@gmail.com <aayushsoodhp@gmail.com>
2025-08-25 16:45:33 +0530

    Scrollbar Hidden

--- src/components/WorkspaceLayout.js
+++ src/components/WorkspaceLayout.js
@@ -28,24 +28,19 @@ export default function WorkspaceLayout({ tools, visualization, subscriptionData
         />
       </div>
       
-      {/* MODIFICATION: Reduced the gap between columns */}
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
-        {/* MODIFICATION: Left column is now narrower to maximize visualization space */}
         <div className="lg:col-span-3 lg:sticky lg:top-24 lg:self-start">
+          {/* MODIFICATION: Using the plain CSS class 'hide-scrollbar' */}
           <div className="
             space-y-4 
-            lg:max-h-[calc(100vh-8rem)] lg:overflow-y-auto
-            [&::-webkit-scrollbar]:w-2
-            [&::-webkit-scrollbar-track]:bg-transparent
-            [&::-webkit-scrollbar-thumb]:rounded-full
-            [&::-webkit-scrollbar-thumb]:bg-neutral-700
-            hover:[&::-webkit-scrollbar-thumb]:bg-neutral-600
+            lg:max-h-[calc(100vh-8rem)] 
+            lg:overflow-y-auto 
+            hide-scrollbar
           ">
             {tools}
           </div>
         </div>
         
-        {/* MODIFICATION: Right column is now wider */}
         <div className="mt-6 lg:mt-0 lg:col-span-9 bg-black rounded-2xl shadow-lg border border-neutral-800 overflow-hidden min-h-[500px] lg:min-h-full h-full">
           {visualization}
         </div>
